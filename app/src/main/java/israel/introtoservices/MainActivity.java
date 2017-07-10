cff35aa13e20d76145bd94a92fe2a03cfcde0d38
hackeru <hackerudroids@gmail.com>
2017-07-10 21:20:43 +0300

    new

--- app/src/main/java/israel/introtoservices/MainActivity.java
+++ app/src/main/java/israel/introtoservices/MainActivity.java
@@ -1,31 +1,41 @@
 package israel.introtoservices;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.sip.SipSession;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static android.R.attr.filter;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+        listenToResult("ReshetB");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -47,5 +57,25 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.fab)
+    public void onViewClicked() {
+        Intent intent = new Intent(this, NotificationService.class);
+
+        startService(intent);
+    }
+
+    private void listenToResult(String reshetB) {
+        LocalBroadcastManager mgr = LocalBroadcastManager.getInstance(this);
+        BroadcastReceiver listener = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String html = intent.getStringExtra("html");
+                Toast.makeText(context, html, Toast.LENGTH_SHORT).show();
+            }
+        };
+        IntentFilter = new  IntentFilter("ReshetB");
+        mgr.registerReceiver(listener, filter);
     }
 }
--- app/src/main/java/israel/introtoservices/NotificationService.java
+++ app/src/main/java/israel/introtoservices/NotificationService.java
@@ -1,16 +1,71 @@
 package israel.introtoservices;
 
-import android.app.Service;
+import android.app.IntentService;
 import android.content.Intent;
-import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
-public class NotificationService extends Service {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class NotificationService extends IntentService {
+
+    public static final String TAG = "NotificationService";
+
     public NotificationService() {
+        super(TAG);
     }
+
+    BufferedReader reader = null;
+    HttpURLConnection con = null;
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    protected void onHandleIntent(@Nullable Intent intent) {
+        try {
+            URL url = new URL("https://github.com/JakeWharton/butterknife");
+            con = (HttpURLConnection) url.openConnection();
+            InputStream in = con.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder result = new StringBuilder();
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+                result.append(line).append("\n");
+            }
+
+            Log.d(TAG, "onHandleIntent:" + result);
+            shout(result.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                con.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
+
+    private void shout(String s) {
+        LocalBroadcastManager mgr = LocalBroadcastManager.getInstance(this);
+        Intent intent = new Intent("Reshet B");
+        intent.putExtra("html", s);
+
+        mgr.sendBroadcast(intent);
+    }
+
 }
+
